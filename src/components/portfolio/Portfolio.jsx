c9db19fefac03a368869a008a824965e5e8e59f1
MiaKun <jeremia.susanto717@gmail.com>
2025-06-19 21:58:48 +0700

    Update portfolio images from SVG to JPG format
    - Replace work-1.svg to work-6.svg with corresponding JPG files
    - Update Portfolio.jsx imports to use JPG images
    - Remove unused avatar SVG files
    - Improve portfolio visual quality with JPG images

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './portfolio.css';
-import Work1 from '../../assets/work-1.svg';
-import Work2 from '../../assets/work-2.svg';
-import Work3 from '../../assets/work-3.svg';
-import Work4 from '../../assets/work-4.svg';
-import Work5 from '../../assets/work-5.svg';
-import Work6 from '../../assets/work-6.svg';
+import Work1 from '../../assets/work-1.jpg';
+import Work2 from '../../assets/work-2.jpg';
+import Work3 from '../../assets/work-3.jpg';
+import Work4 from '../../assets/work-4.jpg';
+import Work5 from '../../assets/work-5.jpg';
+import Work6 from '../../assets/work-6.jpg';
 
 const Menu = [  {
     id: 1,
